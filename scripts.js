dcdaf5309505c70984dfe2d4e5a27ca6e389fc6d
Gabriel Gremio <gabrieldgremio@gmail.com>
2022-11-20 16:11:37 +0800

    added schedule and animations

--- scripts.js
+++ scripts.js
@@ -47,7 +47,7 @@ searchButton.onclick = search
  * inject bookmarks into html
  */
 
-const bookmarks = [{"id":"SjMaholPdjS0zGNX","label":"School","bookmarks":[{"id":"9YKE5lHBrGrZTMLz","label":"schoology","url":"https://csabacolod.schoology.com/login?&school=2685318683"},{"id":"TTAmu9Zo6qAWUwar","label":"gmail","url":"gmail.com"},{"id":"XXCD2G52D3j4iF13","label":"notion","url":"https://www.notion.so/Dashboard-5f16a3668c0d4165935532a80aba9b79"}]},{"id":"5JKaBY6tOdRSq6zt","label":"Humanity","bookmarks":[{"id":"0gmgQGmOY5VfOtjs","label":"messenger","url":"https://www.messenger.com/"},{"id":"SwTdqMAEzxlLZdE6","label":"discord","url":"https://discord.com/app"},{"id":"isNNZAB2PtTZXoa5","label":"myanimelist","url":"https://myanimelist.net/"}]},{"id":"kjewSEOXZIu4G0IK","label":"CatchUp","bookmarks":[{"id":"1fGIQrxA7WukFOiV","label":"shows and movies","url":"https://putlocker.pe/"},{"id":"YDvUuIStGEJ9Munw","label":"anime :0","url":"http://gogoanime.ar/"}]}]
+const bookmarks = [{"id":"SjMaholPdjS0zGNX","label":"School","bookmarks":[{"id":"9YKE5lHBrGrZTMLz","label":"schoology","url":"https://csabacolod.schoology.com/login?&school=2685318683"},{"id":"TTAmu9Zo6qAWUwar","label":"gmail","url":"gmail.com"},{"id":"XXCD2G52D3j4iF13","label":"notion","url":"https://www.notion.so/Dashboard-5f16a3668c0d4165935532a80aba9b79"}]},{"id":"5JKaBY6tOdRSq6zt","label":"Humanity","bookmarks":[{"id":"0gmgQGmOY5VfOtjs","label":"messenger","url":"https://www.messenger.com/"},{"id":"SwTdqMAEzxlLZdE6","label":"discord","url":"https://discord.com/app"},{"id":"isNNZAB2PtTZXoa5","label":"myanimelist","url":"https://myanimelist.net/"}]},{"id":"kjewSEOXZIu4G0IK","label":"CatchUp","bookmarks":[{"id":"1fGIQrxA7WukFOiV","label":"shows and movies","url":"https://putlocker.pe/"},{"id":"YDvUuIStGEJ9Munw","label":"anime :0","url":"http://gogoanime.ar/"},{"id":"YDvUuIStGEJ9Munw","label":"youtub","url":"http://youtube.com/"}]}]
 
 const createGroupContainer = () => {
   const container = document.createElement("div")
@@ -92,3 +92,23 @@ const injectBookmarks = () => {
 }
 
 injectBookmarks()
+
+function switchDiv() {
+  var x = document.getElementById("newTab");
+  var y = document.getElementById('class')
+  if (x.style.top != '100vh') {
+    x.style.top = '100vh';
+    y.style.maxWidth = '70vw';
+    setTimeout(() => {
+      y.style.boxShadow = 'var(--surface-shadow)';
+    }, 500);
+    x.style.boxShadow = 'none';
+  } else {
+    x.style.top = '1vh';
+    y.style.maxWidth = 'var(--surface-maxWidth)';
+    y.style.boxShadow = 'none';
+    setTimeout(() => {
+      x.style.boxShadow = 'var(--surface-shadow)';
+    }, 500);
+  }
+}
